a64a2d74d1e1948bb1f1e28a4ae4c80b6dd27525
AKB428 <gandara016+1@gmail.com>
2017-10-18 02:47:56 +0900

    add web detect

---
+++ cvision_webdetection.rb
@@ -0,0 +1,31 @@
+require 'httpclient'
+require 'base64'
+require 'json'
+require 'pp'
+
+def request_json(content)
+  {
+      requests: [{
+                     image: {
+                         content: content
+                     },
+                     features: [{
+                                    type: 'WEB_DETECTION',
+                                    maxResults: 20
+                                }]
+                 }]
+  }.to_json
+end
+
+def request(file_path)
+  endpoint_uri = "https://vision.googleapis.com/v1/images:annotate?key=#{@key}"
+  http_client = HTTPClient.new
+  content = Base64.strict_encode64(File.new(file_path, 'rb').read)
+  response = http_client.post_content(endpoint_uri, request_json(content), 'Content-Type' => 'application/json')
+  JSON.parse(response)['responses'].first
+end
+
+file_path = ARGV[0]
+@key = ARGV[1]
+
+pp request(file_path)
